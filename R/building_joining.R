4c62f59c80995c8b9b84df985688d65ac9148396
Matt Brousil <37380883+mbrousil@users.noreply.github.com>
2026-03-09 16:53:56 -0700

    Working on build_sr function. Mem lim probs
    Investigate other means of stacking

--- R/building_joining.R
+++ R/building_joining.R
@@ -13,12 +13,17 @@
 #'
 #' @param which_sr String. Options are "siteSR" or "lakeSR", indicating which of
 #' the two SR products should be built.
-#' @param sr_location String. Path to location where SR files are being stored.
+#' @param sr_location String. Path to location where SR files are saved.
+#' @param algal_mask Logical. If TRUE, the algal mask version of the dataset (DSWE1a)
+#' will be expected in `sr_location`. Otherwise DSWE1 is expected (i.e., FALSE).
 #' @param sr_files Optional. A vector of filenames (five at most) with siteSR or
-#' lakeSR files, like would be saved when runing `download_lakeSR()` or `download_siteSR()`.
+#' lakeSR files, like would be saved when running `download_lakeSR()` or `download_siteSR()`.
+#' Should *not* include the directory provided in `sr_location`.
 #' @param save Logical. Should the built SR dataset be saved locally? Defaults to TRUE.
 #' @param save_location String. If save == TRUE, the path to the folder where the
-#' output file should be saved.
+#' output file should be saved. If a name ending in ".feather" is provided as part
+#' of the path then this is the name that the file will be saved with. Otherwise,
+#' a default name will be used by the function. It will always be a .feather file.
 #'
 #' @returns
 #' @export
@@ -27,21 +32,85 @@
 #' \dontrun{
 #'
 #' }
-build_sr <- function(which_sr, sr_location, sr_files = NULL, save = TRUE, save_location){
-
+build_sr <- function(which_sr, sr_location, algal_mask, sr_files = NULL,
+                     save = TRUE, save_location){
   # Confirm correct use of SR tag
   if(!(which_sr == "lakeSR" | which_sr == "siteSR")){
     stop("Input for which_sr argument is not valid. Must be 'lakeSR' or 'siteSR'.")
   }
 
-  # Confirm file existence
+  # Confirm correct use of algal_mask
+  if(!is.logical(algal_mask)){
+    stop("Input for algal_mask argument is not a logical value. Must be TRUE or FALSE.")
+  }
+
+  # Potential default SR path names
+  # String for SR + mask combo
+  input_string <- paste0(
+    which_sr,
+    "_",
+    # T/F algal mask
+    switch(
+      as.character(algal_mask),
+      "TRUE" = "DSWE1a",
+      "FALSE" = "DSWE1"
+    )
+  )
+
+  # Get default filename vectors to use
+  default_filenames <- switch(
+    input_string,
+    "siteSR_DSWE1" = c("siteSR_Landsat4_DSWE1_2025-06-06.feather",
+                       "siteSR_Landsat5_DSWE1_2025-06-06.feather",
+                       "siteSR_Landsat7_DSWE1_2025-06-06.feather",
+                       "siteSR_Landsat8_DSWE1_2025-06-06.feather",
+                       "siteSR_Landsat9_DSWE1_2025-06-06.feather"),
+    "siteSR_DSWE1a" = c("siteSR_Landsat4_DSWE1a_2025-06-06.feather",
+                        "siteSR_Landsat5_DSWE1a_2025-06-06.feather",
+                        "siteSR_Landsat7_DSWE1a_2025-06-06.feather",
+                        "siteSR_Landsat8_DSWE1a_2025-06-06.feather",
+                        "siteSR_Landsat9_DSWE1a_2025-06-06.feather"),
+    "lakeSR_DSWE1" = c("lakeSR_Landsat4_DSWE1_2025-06-04.feather",
+                       "lakeSR_Landsat5_DSWE1_2025-06-04.feather",
+                       "lakeSR_Landsat7_DSWE1_2025-06-04.feather",
+                       "lakeSR_Landsat8_DSWE1_2025-06-04.feather",
+                       "lakeSR_Landsat9_DSWE1_2025-06-04.feather"),
+    "lakeSR_DSWE1a" = c( "lakeSR_Landsat4_DSWE1a_2025-06-04.feather",
+                         "lakeSR_Landsat5_DSWE1a_2025-06-04.feather",
+                         "lakeSR_Landsat7_DSWE1a_2025-06-04.feather",
+                         "lakeSR_Landsat8_DSWE1a_2025-06-04.feather",
+                         "lakeSR_Landsat9_DSWE1a_2025-06-04.feather")
+  )
+
+  # Confirm file existence, standardize their names once confirmed
+  # 1. If file path vector was provided
   if(!is.null(sr_files)){
     if(!all(file.exists(file.path(sr_location, sr_files)))) {
       stop("Some or all files in sr_files were not detected in sr_location.")
+    } else{
+      file_list <- file.path(sr_location, sr_files)
+    }
+    # 2. If file path vector wasn't provided (expect default names)
+  } else if(is.null(sr_files)){
+    if(!all(file.exists(file.path(sr_location, default_filenames)))){
+      stop("Some or all expected SR files were not detected in sr_location.")
+    } else{
+      file_list <- file.path(sr_location, default_filenames)
     }
   }
 
-  # Confirm file contents (i.e., col names)
+  message("A series of large files will now be imported. It may take several minutes.")
+
+  # Read files and stack
+  sr_list <- purrr::map(.x = file_list,
+                        .f = arrow::read_feather) %>%
+    dplyr::bind_rows()
+
+  # Now export if requested by user
+  if(save){
+
+  }
+
 }
 
 #' Join AquaMatch WQP dataset to siteSR
